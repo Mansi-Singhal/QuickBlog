82384e716bd429bffd80c6e70f47ab73ce5a8c5e
Mansi Singhal <manshasinghal143@gmail.com>
2025-06-22 22:18:24 +0530

    Update Blog.jsx

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -75,7 +75,7 @@ const Blog = () => {
         <p className='inline-block py-1 px-4 rounded-full mb-6 broder text-sm border-primary/35 bg-primary/5 font-medium text-primary'>Michael Brown</p>
       </div>
 
-      <div className='mx-5max-w-5xl md:mx-auto my-10 mt-6'>
+      <div className='mx-5 max-w-5xl md:mx-auto my-10 mt-6'>
         <img src={data.image} alt="" className='rounded-3xl mb-5' />
         <div dangerouslySetInnerHTML={{ __html: data.description }} className='rich-text max-w-3xl mx-auto'>
         </div>
